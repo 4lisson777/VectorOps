cc006fe69f56657d49e3a416befecf7ba6d83e0c
Alisson Santos <alissonlima529@gmail.com>
2026-04-25 19:05:59 -0300

    chore: merge migrations into initial schema

--- apps/web/prisma/migrations/20260406090904_add_tv_config/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "tv_config" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "isEnabled" BOOLEAN NOT NULL DEFAULT true,
-    "refreshInterval" INTEGER NOT NULL DEFAULT 30
-);
